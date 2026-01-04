ed23a5bb9e2b916f28d614d0aa5c6842c0ff6f8c
priyanshuPJ20 <priyanshu19.kpl@gmail.com>
2026-01-04 15:20:14 +0530

    Hospital management Project Setup

---
+++ HM Project/hospitalManagement/hospitalManagement/src/test/java/com/priyanshu/hospitalManagement/HospitalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyanshu.hospitalManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
